902fdcbd5175f040139ae8af79bcbdd29f71e5e4
Griffin <mcdgri02@evergreen.edu>
2017-11-07 12:31:45 -0800

    Finished lab 4.

--- kern/test/usertest1.c
+++ kern/test/usertest1.c
@@ -7,6 +7,11 @@ int usertest1(int nargs, char **args)
   (void)nargs;
   (void)args;
   kprintf("This is the user test.\n");
+  for(int i = 0; i < nargs; ++i) {
+    kprintf(args[i]);
+    kprintf(" ");
+  }
+  kprintf("\n");
   kprintf("User test complete.\n");
   return 0;
 }
